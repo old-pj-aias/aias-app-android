3e5f0f2c773654e1977a16cbd5e811ae9ced715a
akakou <akakou571@gmail.com>
2020-08-27 12:51:53 +0900

    [add] setSubset

--- app/src/main/java/com/project_aias/aias/Aias.kt
+++ app/src/main/java/com/project_aias/aias/Aias.kt
@@ -3,6 +3,7 @@ package com.aias.aias
 object Aias {
     external fun new(input: String?)
     external fun blind(input: String?): String?
+    external fun setSubset(input: String?)
 
     init {
         System.loadLibrary("aiascore")
--- app/src/main/java/com/project_aias/aias/MainActivity.kt
+++ app/src/main/java/com/project_aias/aias/MainActivity.kt
@@ -16,6 +16,7 @@ hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
 /wIDAQAB
 -----END PUBLIC KEY-----
 """
+    val subset = """{"subset":[2,7,8,11,12,13,14,18,23,25,26,27,28,30,31,33,35,38,39,40,41,42,43,44,46,48,54,55,56,58,62,65,68,69,70,71,74,75,77,78],"complement":[1,3,4,5,6,9,10,15,16,17,19,20,21,22,24,29,32,34,36,37,45,47,49,50,51,52,53,57,59,60,61,63,64,66,67,72,73,76,79,80]}"""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +24,9 @@ hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
 
         Aias.new(publicKey);
         val blindedDigest = Aias.blind("aaaa");
+        Toast.makeText(this, blindedDigest, Toast.LENGTH_LONG).show();
 
-        Toast.makeText(this, blindedDigest, Toast.LENGTH_SHORT).show();
+        Aias.setSubset(subset);
+
     }
 }
