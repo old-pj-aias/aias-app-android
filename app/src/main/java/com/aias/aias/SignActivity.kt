a9e6212ffb70c1bbe7ea1c01d03815649ed26dc8
akakou <akakou571@gmail.com>
2020-08-31 17:49:34 +0900

    [add] Send blinded digest

--- app/src/main/java/com/aias/aias/SignActivity.kt
+++ app/src/main/java/com/aias/aias/SignActivity.kt
@@ -1,12 +1,12 @@
 package com.aias.aias
 
-import android.app.PendingIntent.getActivity
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.github.kittinunf.fuel.Fuel
 import kotlin.concurrent.thread
 
 
@@ -21,11 +21,11 @@ hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
 /wIDAQAB
 -----END PUBLIC KEY-----
 """
-    val subset = """{"subset":[2,7,8,11,12,13,14,18,23,25,26,27,28,30,31,33,35,38,39,40,41,42,43,44,46,48,54,55,56,58,62,65,68,69,70,71,74,75,77,78],"complement":[1,3,4,5,6,9,10,15,16,17,19,20,21,22,24,29,32,34,36,37,45,47,49,50,51,52,53,57,59,60,61,63,64,66,67,72,73,76,79,80]}"""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone)
+
+        val text = intent.getStringExtra("message")
 
         AlertDialog.Builder(this)
             .setTitle("warning")
@@ -34,14 +34,20 @@ hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
                 thread {
                     Aias.new(publicKey, publicKey);
 
-                    val text = intent.getStringExtra("message")
                     val blindedDigest = Aias.blind(text);
+
+                    val (_, response, _) = Fuel.post("http://192.168.0.24:8080/ready")
+                        .body(blindedDigest!!)
+                        .response()
+
+                    val subset = String(response.data)
                     Aias.setSubset(subset)
+
                     val checkParam = Aias.generateCheckParameter();
 
                     runOnUiThread {
-                        Toast.makeText(this, blindedDigest, Toast.LENGTH_LONG).show();
-                        Toast.makeText(this, checkParam, Toast.LENGTH_LONG).show()
+                        Toast.makeText(this, subset, Toast.LENGTH_LONG).show();
+                        Toast.makeText(this, checkParam, Toast.LENGTH_LONG).show();
                     }
                 }
 
