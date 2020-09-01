706faa7e91221d76b41a31a86c580315f5ed3317
akakou <akakou571@gmail.com>
2020-09-01 16:54:27 +0900

    [add] Selecter

--- app/src/main/java/com/aias/aias/SignActivity.kt
+++ app/src/main/java/com/aias/aias/SignActivity.kt
@@ -2,9 +2,10 @@ package com.aias.aias
 
 import android.app.Activity
 import android.content.DialogInterface
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +14,7 @@ import kotlin.concurrent.thread
 
 
 class SignActivity : AppCompatActivity(), View.OnClickListener {
-    val publicKey = """-----BEGIN PUBLIC KEY-----
+    val signerKey = """-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxXo2zWkciUEZBcm/Exk8
 Zac8NWskP59EAVFlO218xIXOV0FfphPB/tnbQh7GDXddo7XVEptHdHXyJlXXLihb
 9vXbUZF2NDFLOhgDv7pa72VNLbw+jKR/FlsDtwv/bv7ZDqq+n79uavuJ8giX3qCf
@@ -23,6 +24,35 @@ hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
 /wIDAQAB
 -----END PUBLIC KEY-----"""
 
+    val ejPubkeys = listOf("""-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxXo2zWkciUEZBcm/Exk8
+Zac8NWskP59EAVFlO218xIXOV0FfphPB/tnbQh7GDXddo7XVEptHdHXyJlXXLihb
+9vXbUZF2NDFLOhgDv7pa72VNLbw+jKR/FlsDtwv/bv7ZDqq+n79uavuJ8giX3qCf
++mtBmro7hG5AVve3JImhvA0FvTKJ0xCYUYw02st08He5RwFAXQK8G2cwahp+5ECH
+MDdfFUaoxMfRN/+Hl9iqiJovKUJQ3545N2fDYdd0eqSlqL1N5xJxYX1GDMtGZgME
+hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
+/wIDAQAB
+-----END PUBLIC KEY-----""",
+        """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxXo2zWkciUEZBcm/Exk8
+Zac8NWskP59EAVFlO218xIXOV0FfphPB/tnbQh7GDXddo7XVEptHdHXyJlXXLihb
+9vXbUZF2NDFLOhgDv7pa72VNLbw+jKR/FlsDtwv/bv7ZDqq+n79uavuJ8giX3qCf
++mtBmro7hG5AVve3JImhvA0FvTKJ0xCYUYw02st08He5RwFAXQK8G2cwahp+5ECH
+MDdfFUaoxMfRN/+Hl9iqiJovKUJQ3545N2fDYdd0eqSlqL1N5xJxYX1GDMtGZgME
+hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
+/wIDAQAB
+-----END PUBLIC KEY-----""",
+        """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxXo2zWkciUEZBcm/Exk8
+Zac8NWskP59EAVFlO218xIXOV0FfphPB/tnbQh7GDXddo7XVEptHdHXyJlXXLihb
+9vXbUZF2NDFLOhgDv7pa72VNLbw+jKR/FlsDtwv/bv7ZDqq+n79uavuJ8giX3qCf
++mtBmro7hG5AVve3JImhvA0FvTKJ0xCYUYw02st08He5RwFAXQK8G2cwahp+5ECH
+MDdfFUaoxMfRN/+Hl9iqiJovKUJQ3545N2fDYdd0eqSlqL1N5xJxYX1GDMtGZgME
+hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
+/wIDAQAB
+-----END PUBLIC KEY-----""");
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone)
@@ -31,55 +61,73 @@ hHR6ntdfm7r43HDB4hk/MJIsNay6+K9tJBiz1qXG40G4NjMKzVrX9pi1Bv8G2RnP
         AlertDialog.Builder(this)
             .setTitle("warning")
             .setMessage("Are you trust calling app?")
-            .setPositiveButton("OK") { dialog, which ->
+            .setPositiveButton("OK") { _, _ ->
 
             }
-            .setNegativeButton("Cancel", { dialogInterface: DialogInterface, i: Int ->
+            .setNegativeButton("Cancel") { _: DialogInterface, i: Int ->
                 setResult(Activity.RESULT_CANCELED)
                 finish()
-            })
+            }
             .show()
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+        val adapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        for (i in 1..ejPubkeys.size) {
+            adapter.add(i.toString())
+        }
+
+        spinner.adapter = adapter
     }
 
     override fun onClick(v: View?) {
         val intent = intent
         val text = intent.getStringExtra("message")
 
-        when (v!!.getId()) {
+        when (v!!.id) {
             R.id.submit_phone -> {
                 setContentView(R.layout.activity_sms_code)
             }
-        }
 
-        thread {
-            Aias.new(publicKey, publicKey);
+            R.id.submit_code -> {
+                thread {
+                    val spinner = findViewById<Spinner>(R.id.spinner)
 
-            val blindedDigest = Aias.blind(text);
+                    val ejIndex = spinner.selectedItem as String;
+                    val ejPubkey = ejPubkeys.get(ejIndex.toInt());
 
-            val (_, readyResponse, _) = Fuel.post("http://192.168.0.24:8080/ready")
-                .body(blindedDigest!!)
-                .response()
+                    Aias.new(signerKey, ejPubkey);
 
-            val subset = String(readyResponse.data)
-            Aias.setSubset(subset)
+                    val blindedDigest = Aias.blind(text);
 
-            val checkParam = Aias.generateCheckParameter();
+                    val (_, readyResponse, _) = Fuel.post("http://192.168.0.24:8080/ready")
+                        .body(blindedDigest!!)
+                        .response()
 
-            val (_, signResponse, _) = Fuel.post("http://192.168.0.24:8080/sign")
-                .body(checkParam!!)
-                .response()
+                    val subset = String(readyResponse.data)
+                    Aias.setSubset(subset)
 
-            val blindSignature = String(signResponse.data)
-            val signature = Aias.unblind(blindSignature)
+                    val checkParam = Aias.generateCheckParameter();
 
-            runOnUiThread {
-                Toast.makeText(this, signature, Toast.LENGTH_LONG).show();
+                    val (_, signResponse, _) = Fuel.post("http://192.168.0.24:8080/sign")
+                        .body(checkParam!!)
+                        .response()
 
-                intent.putExtra("message", signature)
-                setResult(Activity.RESULT_OK, intent)
-                finish()
+                    val blindSignature = String(signResponse.data)
+                    val signature = Aias.unblind(blindSignature)
+
+                    runOnUiThread {
+                        Toast.makeText(this, signature, Toast.LENGTH_LONG).show();
+
+                        intent.putExtra("message", signature)
+                        setResult(Activity.RESULT_OK, intent)
+                        finish()
+                    }
+                }
             }
         }
     }
-
 }
